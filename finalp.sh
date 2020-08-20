9a81eb62376e1733ea9a8dca38215a331d94d539
bhargavk-dev <68079013+bhargavk-dev@users.noreply.github.com>
2020-08-20 14:32:11 -0400

    Update finalp.sh

--- finalp.sh
+++ finalp.sh
@@ -2,6 +2,7 @@
 set -xe
 
 sudo apt-get update
+sudo apt install python3-pip
 sudo apt-get -y install ansible
 sudo apt-get -y install git
 git clone "https://github.com/bhargavk-dev/project-kube.git"
